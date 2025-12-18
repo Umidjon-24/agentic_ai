937fa0d742f540761f3858609c0cfcbc3bb4cb17
Umidjon-24 <u.xusanov@newuu.uz>
2025-12-18 16:54:31 +0500

    lesson-3

--- homeworks/lesson-2/chatbot.py
+++ homeworks/lesson-2/chatbot.py
@@ -1,2 +1,15 @@
 from google import genai
-import sqlite3
+from google.genai import types
+from sqlalchemy import create_engine, text, inspect
+import pyodbc
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Hi"
+)
+
---
+++ homeworks/lesson-3/fn_call.py
@@ -0,0 +1,41 @@
+from google import genai
+from google.genai import types
+from sqlalchemy import create_engine, text, inspect
+import pyodbc
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+engine = create_engine(
+    "mssql+pyodbc://@DESKTOP-EQ1S466/sales"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+    "&trusted_connection=yes"
+)
+
+def tool_db(query: str):
+    """Run a read-only SQL Server SELECT query.
+    Args:
+        query
+    """
+    with engine.connect() as conn:
+        result = conn.execute(text(query))
+        rows = result.fetchall()
+    if not rows:
+        return "No results found."
+
+    return [dict(row._mapping) for row in rows]
+
+config = types.GenerateContentConfig(
+    tools=[tool_db]
+)  # Pass the function itself
+
+# Make the request
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Give me product names with their prices",
+    config=config,
+)
+
+print(response.text) 
